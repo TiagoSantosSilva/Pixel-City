8e8069850abae71eff74a7ae2b6315fe8800bcde
Tiago Silva <1140437@isep.ipp.pt>
2017-12-19 12:44:03 +0000

    Map View Allow changes
    Disabled Zooming, Rotating, 3D View and Compass.

--- Pixel-City/Controllers/MapViewController.swift
+++ Pixel-City/Controllers/MapViewController.swift
@@ -15,6 +15,7 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
---
+++ Pixel-City/Extensions/MapViewControllerExtension.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewControllerExtension.swift
+//  Pixel-City
+//
+//  Created by Tiago Santos on 19/12/17.
+//  Copyright © 2017 Tiago Santos. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+extension MapViewController: MKMapViewDelegate {
+    
+}
