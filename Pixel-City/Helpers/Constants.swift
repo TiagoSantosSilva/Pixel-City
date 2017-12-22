047c22f6b8e3ba01dfb0cbc96ff547f30e876191
Tiago Santos <tiagossilva_95@hotmail.com>
2017-12-22 16:44:12 +0000

    Created the URL to retrieve the images from Flickr.

--- Pixel-City/Helpers/Constants.swift
+++ Pixel-City/Helpers/Constants.swift
@@ -8,6 +8,10 @@
 
 import Foundation
 
+let FlickrApiKey = "c649831ffb6122070cdde91bcdcfedf0"
 let DroppablePinReuseIdentifier = "droppablePin"
 let PhotoCellReuseIdentifier = "photoCell"
 
+func flickrUrl(forApiKey key: String, withAnnotation annotation: DroppablePin, andNumberOfPhotos numberOfPhotos: Int) -> String {
+    return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(key)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=1&radius_units=km&per_page=\(numberOfPhotos)&format=json&nojsoncallback=1"
+}
