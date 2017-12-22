026a0f12aa2eb3854ae22b1e8f7f7a63c7788733
Tiago Santos <tiagossilva_95@hotmail.com>
2017-12-22 22:44:23 +0000

    Creation of the Pop Image View Controller.

---
+++ Pixel-City/Controllers/PopImageViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PopImageViewController.swift
+//  Pixel-City
+//
+//  Created by Tiago Santos on 22/12/17.
+//  Copyright © 2017 Tiago Santos. All rights reserved.
+//
+
+import UIKit
+
+class PopImageViewController: UIViewController {
+
+    @IBOutlet weak var popImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
